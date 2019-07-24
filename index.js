3595f5f4f994642b253588fe203f03acbe49d250
Hanna Mohr <hmo@exec-intern.exec-software.de>
2019-07-24 10:16:15 +0200

    schritte bis ausschließlich umbau auf angular erledigt

--- index.js
+++ index.js
@@ -1,72 +1,93 @@
-var url = 'https://jsonplaceholder.typicode.com/users/1';
-var data = [{
-        	    username: 'example', 
-                userId: 123456789},
-            {
-               username: '1234',
-               userId: 1, 
-            }];
-
-fetch(url, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-    headers:{
-        'Content-Type': 'application/json'
-    }
-}).then(res => res.json())
-.then (response => console.log('Success', JSON.stringify(response)))
-.catch(error => console.error('Error',error));
+function loadDoc() {
+    var url = 'https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+        .then(response => response.json())
+        .then(function(response){
+            console.log(response);
+            var todoListe = response;
+            var container = document.getElementById('container');
+            for (var i = 0; i < todoListe.length; i++){
+                var todo = todoListe[i];
+                container.appendChild(createTodo(todo));
+            }
+        })
+}       
 
 
-// Funktion zum Erscheinen des Eingabefeldes
-function neuanlegen(){
-    var buttonHinzufuegen = document.getElementById("hinzufuegen");
-    buttonHinzufuegen.onclick = function() {
-        var hinzufuegen = document.getElementById("hinzu"); 
-        hinzufuegen.style.display = "block";
-        console.log("angeklickt");
+function createTodo(todo) {
+    var group = document.createElement('div');
+    group.classList.add("group");
+    var container = document.getElementById("container");
+    container.appendChild(group);
+    //todo Element erzeugen
+    var todoElem = document.createElement('input');
+    todoElem.type = "checkbox";
+    todoElem.name = todo.title;
+    todoElem.id = todo.id;
+
+    //todo Element group hinzufügen
+    group.appendChild(todoElem);
+    var text = document.createTextNode(" " + todoElem.name);
+    group.appendChild(text);
+  
+    // Loeschenbutton erzeugen
+    var loeschen = document.createElement("button");
+    loeschen.classList.add('button');
+    loeschen.id = todoElem.id + "loeschen";
+    // Loeschenbutton zu group hinzufuegen 
+    group.appendChild(loeschen);
+    loeschen.appendChild(document.createTextNode("löschen"));
+
+    // Element loeschen
+    var entfernen = document.getElementById(todoElem.id+"loeschen");
+    entfernen.onclick = function(){
+        console.log("entfernen");
+        container.removeChild(group);
     };
+    return group;
 }
 
 function bearbeiten(){
-    var bestaetigen = document.getElementById("be");
-    bestaetigen.onclick = function(){
-        console.log("bestätigen angeklickt");
+    var hinzu = document.getElementById("hinzufuegen");
+    hinzu.onclick = function(){
+        console.log("hinzufügen angeklickt");
         // neuen Eintrag einfuegen 
         var neuerEintrag = document.forms["eintrag"]["neu"].value;
-        console.log("neuer Eintrag "+neuerEintrag);
-        var container = document.getElementById("container");
+        console.log("neuer Eintrag " +  neuerEintrag);
+        var group = document.createElement('div');
         // input Element erzeugen 
         var input = document.createElement("input");
         input.type = "checkbox";
         input.name = neuerEintrag;
         input.id = neuerEintrag;
         console.log(input)
-        // input Element zu container hinzufuegen
-        container.appendChild(input);
+        // input Element zu group hinzufuegen
+        var container = document.getElementById("container");
+        container.appendChild(group);
+        group.appendChild(input);
         var text = document.createTextNode(" " + neuerEintrag);
-        container.appendChild(text);
+        group.appendChild(text);
         // Loeschenbutton erzeugen
         var loeschen = document.createElement("button");
-        loeschen.id = neuerEintrag +"loeschen";
+        loeschen.classList.add('button');
+        loeschen.id = neuerEintrag+ "loeschen";
         console.log(loeschen);
-        // Loeschenbutton zu container hinzufuegen 
-        container.appendChild(loeschen);
+        // Loeschenbutton zu group hinzufuegen 
+        group.appendChild(loeschen);
         loeschen.appendChild(document.createTextNode("löschen"));
-        container.appendChild(document.createElement("br"));
 
         // Element loeschen
-        var entfernen = document.getElementById(neuerEintrag+"loeschen");
+        var entfernen = document.getElementById(neuerEintrag + "loeschen");
         var elementloeschen = document.getElementById(neuerEintrag);
         entfernen.onclick = function(){
             console.log("entfernen");
-            container.removeChild(entfernen);
-            container.removeChild(elementloeschen); 
-            container.removeChild(text);
-            container.removeChild(container.lastChild);
+            group.removeChild(entfernen);
+            group.removeChild(elementloeschen); 
+            group.removeChild(text);
         };
     };
 }
 
-neuanlegen();
+
 bearbeiten(); 
+loadDoc();
